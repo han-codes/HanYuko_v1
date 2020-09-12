ab21fd614ad812bb12e36a396c8ce24df77128ff
Jacob Pernell <jacob.pernell@gmail.com>
2020-09-12 15:31:42 -0700

    Initial project commit
    Created fresh Xcode project and updated .gitignore to include .DS_Store

---
+++ HanYuko_v1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HanYuko_v1
+//
+//  Created by Jacob Pernell on 9/12/20.
+//  Copyright © 2020 Jacob Pernell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
