1887fa60405d1592b6f7aa3da8c98099db9c2ba4
Jacob Pernell <jacob.pernell@gmail.com>
2020-09-14 14:35:58 -0700

    Reverting back to early on before merging to see if I can start fresh

--- HanYuko_v1/ViewController.swift
+++ HanYuko_v1/ViewController.swift
@@ -8,40 +8,11 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
-    
-    let searchController = UISearchController(searchResultsController: nil)
-    
-    // Font styling attributes
-    let largeTitleFont = UIFont(name: "Rubik-Bold", size: 34) ?? UIFont(name: "Arial", size: 34)
-    let normalTitleFont = UIFont(name: "Rubik-Bold", size: 17) ?? UIFont(name: "Arial", size: 17)
-    let regularFont = UIFont(name: "Rubik-Regular", size: 17) ?? UIFont(name: "Arial", size: 17)
-    let titleColor = UIColor(red: 0.10, green: 0.42, blue: 0.78, alpha: 1.00)
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-                
-        // Set the title of the home screen
-        title = "HanYuko Images"
-        
-        // Enable large header title for the home screen and set its properties
-        navigationController?.navigationBar.prefersLargeTitles = true
-        
-        // The BEEFY title
-        let largeTitleTextAttributes: [NSAttributedString.Key: Any] = [
-            NSAttributedString.Key.font: largeTitleFont!,
-            NSAttributedString.Key.foregroundColor: titleColor
-        ]
-        // When you scroll up
-        let titleTextAttributes: [NSAttributedString.Key: Any] = [
-            NSAttributedString.Key.font: normalTitleFont!,
-            NSAttributedString.Key.foregroundColor: titleColor
-        ]
-        navigationController?.navigationBar.largeTitleTextAttributes = largeTitleTextAttributes
-        navigationController?.navigationBar.titleTextAttributes = titleTextAttributes
-        
-        // Adds search input to navigation area
-        navigationItem.searchController = searchController
+        // Do any additional setup after loading the view.
     }
 
 
--- Pods/Target Support Files/Pods-HanYuko_v1Tests/Pods-HanYuko_v1Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HanYuko_v1Tests : NSObject
-@end
-@implementation PodsDummy_Pods_HanYuko_v1Tests
-@end
