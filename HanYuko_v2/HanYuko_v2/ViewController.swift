dbd2378ea4cf4b3f8d7d7645057c99904ec8e8a6
Jacob Pernell <jacob.pernell@gmail.com>
2020-09-14 14:40:29 -0700

    v2 - Initial commit
    Starting over from scratch. Will copy/paste in old code to recreate what I had so far

---
+++ HanYuko_v2/HanYuko_v2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HanYuko_v2
+//
+//  Created by Jacob Pernell on 9/14/20.
+//  Copyright © 2020 Jacob Pernell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
